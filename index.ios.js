e957dbf25ce99e1f52fc13aaf9109810bcdbbc20
helfi92 <helfi92@gmail.com>
2016-07-17 12:36:05 -0700

    initial markup

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { Text, View, AppRegistry, StyleSheet } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const StopWatch = React.createClass({
+ render: function() {
+   return <View>
+      <View>
+        <View>
+          <Text>
+            00:00.00
+          </Text>
+        </View>
+        <View>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
 
-class stopwatch extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+      <View>
+        <Text>
+          I am a list of Laps
         </Text>
       </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+   </View>
+ },
+ startStopButton: function() {
+   return <View>
+     <Text>
+       Start
+     </Text>
+   </View>
+ },
+ lapButton: function() {
+   return <View>
+     <Text>
+       Lap
+     </Text>
+   </View>
+ }
 });
 
-AppRegistry.registerComponent('stopwatch', () => stopwatch);
+var styles= StyleSheet.create({
+  container: {
+    flex: 1, // Fill the entire screen
+    alignItems: 'stretch'
+  },
+  header: { // Yellow
+    flex: 1
+  },
+  footer: { // Blue
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
